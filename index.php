0d095df5c23f7a2948af633272e74caba37f632a
MrMoovf <anderskozuch1998@gmail.com>
2024-02-09 13:43:11 +0100

    maybe

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require 'vendor/autoload.php';
+
+// echo 'ee';
+use Arostech\AnalyticsProcessing\Processor;
+
+Processor::test();
---
+++ src/Processor.php
@@ -0,0 +1,8 @@
+<?php
+namespace Arostech\AnalyticsProcessing;
+
+class Processor{
+    public static function test(){
+        echo 'i am the processor';
+    }
+}
