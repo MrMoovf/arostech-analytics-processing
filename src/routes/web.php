492fc95a598c57b2b2203180827b7745920ec836
MrMoovf <anderskozuch1998@gmail.com>
2024-04-18 18:51:14 +0200

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
+use Arostech\Models\Request as ModelsRequest;
+
 use Illuminate\Support\Facades\Route;
-use Arostech\Api\ApiController;
- 
+use Illuminate\Http\Request;
 
 // ------------------ ANALYTICS
-Route::post('/analytics', function (){
+Route::post('/analytics', function (Request $request){
+    ModelsRequest::create([
+        'log_type' => 'pageview',
+        'route' => $request->path(),
+        'useragent' => $request->userAgent(),
+        'visitor_id' => crypt($request->ip(),'123'),
+        'referer' => request()->header('referer') ?? 'wasNull'
+    ]);
+    return $response;
     return response('Middleware has run',200);
 });
